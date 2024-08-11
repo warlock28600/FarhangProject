4fb3cdc8c1c73d10f4a8743916840b42c8ca5925
Farhang <farhang.ezziirani@gmail.com>
2024-08-11 14:13:01 -0700

    some work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
+import {BaseComponent} from "./base.component";
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,13 @@ import {Router, RouterOutlet} from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'farhang';
+export class AppComponent extends BaseComponent implements OnInit {
+  title = this.name;
   currentRoute?: string;
   showNavBar = true;
 
   constructor(private router: Router) {
+    super();
   }
 
   ngOnInit() {
